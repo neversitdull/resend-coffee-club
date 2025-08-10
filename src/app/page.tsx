7671a5cc1d6f13487b80d81873f1c596f30fb9ac
Josh Dunsterville <neversitdull@icloud.com>
2025-08-10 15:02:13 -0700

    chore: install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      Resend Coffee Club
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-balance text-center font-bold text-6xl opacity-90">
+        Resend Coffee Club
+      </h1>
+      <p className="max-w-prose text-balance text-center font-medium text-lg opacity-70">
+        Specialty coffee, hand-selected by the Resend team. Delivered straight
+        to your inbox every Wednesday.
+      </p>
     </div>
   );
 }
