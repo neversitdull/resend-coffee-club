c9e02a1bb8c63c4221a49b24e6c19dbc798e83f9
Josh Dunsterville <neversitdull@icloud.com>
2025-08-10 15:16:11 -0700

    feat: add form and simple server action
    install Button and Input components from shadcn
    minor style updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
+  async function subscribe(formData: FormData) {
+    "use server";
+
+    const email = formData.get("email");
+
+    console.log(email);
+  }
+
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-balance text-center font-bold text-6xl opacity-90">
@@ -8,6 +19,10 @@ export default function Home() {
         Specialty coffee, hand-selected by the Resend team. Delivered straight
         to your inbox every Wednesday.
       </p>
+      <form className="mt-10 flex gap-2" action={subscribe}>
+        <Input placeholder="Email" name="email" />
+        <Button>Subscribe</Button>
+      </form>
     </div>
   );
 }
