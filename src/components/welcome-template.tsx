8073690d27e74802997f5fefe78b706f1f38ecaf
Josh Dunsterville <neversitdull@icloud.com>
2025-08-10 21:57:47 -0700

    feat: add welcome email template

---
+++ src/components/welcome-template.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function WelcomeEmailTemplate() {
+  return (
+    <div>
+      <h1>Welcome to the Resend Coffee Club</h1>
+      <p>
+        You'll receive a new email every Wednesday with a featured coffee and
+        the option to purchase it.
+      </p>
+    </div>
+  );
+}
