0ff7370dfd82ea3c85d4d46cbb68829ca87e17d6
Josh Dunsterville <neversitdull@icloud.com>
2025-08-10 21:58:02 -0700

    feat: add scheduled email template

---
+++ src/components/scheduled-template.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function ScheduledEmailTemplate() {
+  return (
+    <div>
+      <h1>Happy Wednesday! This week'd featured coffee is here!</h1>
+      <p>
+        Featuring a light roast coffee from Dune Coffee Roasters out of Santa Barbara, California.
+      </p>
+    </div>
+  );
+}
